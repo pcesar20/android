72eefa0b0456898bbb6b0917f3b708f69ee9caec
Paulo Cesar <paulocesar.araujo.ads@gmail.com>
2020-04-16 21:23:42 -0300

    Gradle e IDE atualizados

--- app/src/main/java/br/com/pauloc/listamercado/MainActivity.java
+++ app/src/main/java/br/com/pauloc/listamercado/MainActivity.java
@@ -1,24 +1,17 @@
 package br.com.pauloc.listamercado;
 
 import android.os.Bundle;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
 import android.view.View;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.*;
 
